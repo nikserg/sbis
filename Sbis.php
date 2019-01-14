52c383c55c5f3afbb8bf2e506c40cad241e84a0c
n.zarubin <n.zarubin@itsirius.ru>
2019-01-14 12:36:00 +0300

    master

---
+++ Sbis.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     * Поддержка API СБИС
+     *
+     *
+     * User: n.zarubin
+     * Date: 14.01.2019
+     * Time: 12:31
+     */
+    namespace nikserg\sbis;
+
+    use Yii;
+    use yii\base\Exception;
+    use yii\helpers\Json;
+    use yii\web\HttpException;
+
+    class Sbis
+    {
+
+    }
